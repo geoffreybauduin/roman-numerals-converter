6a91efe03077d9cf376c9b3d39360f81adbd302a
Geoffrey Bauduin <bauduin.geo@gmail.com>
2016-04-10 16:05:11 +0200

    added tests to handle in-depth subtractions

--- roman/roman.go
+++ roman/roman.go
@@ -5,14 +5,20 @@
 package roman
 
 var sorted_data []int = []int{
-    1000, 500, 100, 50, 10, 5, 1,
+    1000000, 500000, 100000, 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1,
 }
 
 var sorted_substract []int = []int{
-    1000, 100, 10, 1,
+    1000000, 100000, 10000, 1000, 100, 10, 1,
 }
 
 var data map[int]string = map[int]string{
+    1000000: "M̅",
+    500000: "D̅",
+    100000: "C̅",
+    50000: "L̅",
+    10000: "X̅",
+    5000: "V̅",
     1000: "M",
     500: "D",
     100: "C",
@@ -42,15 +48,39 @@ func check_direct (nbr int) (bool, int, string) {
     return false, nbr, ""
 }
 
+func check_direct_order_depth (nbr, order, it int, current_roman string) (bool, int, string) {
+    order_depth := get_order(nbr - order)
+    direct, _, roman_depth := check_direct(order + order_depth)
+    if direct {
+        return check_direct_order_depth(nbr, order + order_depth, it + 1, roman_depth)
+    } else if it > 0 {
+        return true, nbr - order, current_roman       
+    } else {
+        return false, nbr, current_roman
+    }
+}
+
+func check_direct_order (nbr int) (bool, int, string) {
+    order := get_order(nbr)
+    direct, _, roman := check_direct(order)
+    if direct {
+        in_depth, nbr, roman_depth := check_direct_order_depth(nbr, order, 0, roman)
+        if in_depth {
+            return true, nbr, roman_depth
+        }
+        return true, nbr - order, roman
+    }
+    return false, nbr, ""
+}
+
 func process (nbr int) (int, string) {
     direct, nbr, roman := check_direct(nbr)
     if direct {
         return nbr, roman
     }
-    order := get_order(nbr)
-    direct, _, roman = check_direct(order)
+    direct, nbr, roman = check_direct_order(nbr)
     if direct {
-        return nbr - order, roman
+        return nbr, roman
     }
     for _, value := range sorted_data {
         if nbr >= value {
--- roman/roman_test.go
+++ roman/roman_test.go
@@ -10,35 +10,42 @@ import (
 )
 
 func Test1to3 (t *testing.T) {
-    assert.Equal(t, IntToRoman(1), "I", "1 should be I")
-    assert.Equal(t, IntToRoman(2), "II", "2 should be II")
-    assert.Equal(t, IntToRoman(3), "III", "3 should be III")
+    assert.Equal(t, "I", IntToRoman(1), "1 should be I")
+    assert.Equal(t, "II", IntToRoman(2), "2 should be II")
+    assert.Equal(t, "III", IntToRoman(3), "3 should be III")
 }
 
 func Test5to8 (t *testing.T) {
-    assert.Equal(t, IntToRoman(5), "V", "5 should be V")
-    assert.Equal(t, IntToRoman(6), "VI", "6 should be VI")
-    assert.Equal(t, IntToRoman(7), "VII", "7 should be VII")
-    assert.Equal(t, IntToRoman(8), "VIII", "8 should be VIII")
+    assert.Equal(t, "V", IntToRoman(5), "5 should be V")
+    assert.Equal(t, "VI", IntToRoman(6), "6 should be VI")
+    assert.Equal(t, "VII", IntToRoman(7), "7 should be VII")
+    assert.Equal(t, "VIII", IntToRoman(8), "8 should be VIII")
 }
 
 func TestEntireValues (t *testing.T) {
-    assert.Equal(t, IntToRoman(10), "X", "10 should be X")
-    assert.Equal(t, IntToRoman(50), "L", "50 should be L")
-    assert.Equal(t, IntToRoman(100), "C", "100 should be C")
-    assert.Equal(t, IntToRoman(500), "D", "500 should be D")
-    assert.Equal(t, IntToRoman(1000), "M", "1000 should be M")
+    assert.Equal(t, "X", IntToRoman(10), "10 should be X")
+    assert.Equal(t, "L", IntToRoman(50), "50 should be L")
+    assert.Equal(t, "C", IntToRoman(100), "100 should be C")
+    assert.Equal(t, "D", IntToRoman(500), "500 should be D")
+    assert.Equal(t, "M", IntToRoman(1000), "1000 should be M")
+    assert.Equal(t, "V̅", IntToRoman(5000), "5000 should be V̅")
 }
 
 func TestCombined (t *testing.T) {
-    assert.Equal(t, IntToRoman(2016), "MMXVI", "2016 should be MMXVI")
+    assert.Equal(t, "MMXVI", IntToRoman(2016), "2016 should be MMXVI")
 }
 
 func TestSubstractions (t *testing.T) {
-    assert.Equal(t, IntToRoman(4), "IV", "4 should be IV")
-    assert.Equal(t, IntToRoman(9), "IX", "9 should be IX")
-    assert.Equal(t, IntToRoman(40), "XL", "40 should be XL")
-    assert.Equal(t, IntToRoman(49), "IL", "49 should be IL")
-    assert.Equal(t, IntToRoman(59), "LIX", "59 should be LIX")
-    assert.Equal(t, IntToRoman(994), "XMIV", "994 should be XMIV")
+    assert.Equal(t, "IV", IntToRoman(4), "4 should be IV")
+    assert.Equal(t, "IX", IntToRoman(9), "9 should be IX")
+    assert.Equal(t, "XL", IntToRoman(40), "40 should be XL")
+    assert.Equal(t, "IL", IntToRoman(49), "49 should be IL")
+    assert.Equal(t, "LIX", IntToRoman(59), "59 should be LIX")
+    assert.Equal(t, "XDIV", IntToRoman(494), "494 should be XDIV")
+    assert.Equal(t, "XMIV", IntToRoman(994), "994 should be XMIV")
+    assert.Equal(t, "IM", IntToRoman(999), "999 should be IM")
+    assert.Equal(t, "MDCXCIII", IntToRoman(1693), "1693 should be MDCXCIII")
+    assert.Equal(t, "MV̅DCCXLIV", IntToRoman(4744), "4744 should be MV̅DCCXLIV")
+    assert.Equal(t, "XX̅IV", IntToRoman(9994), "9994 should be XX̅IV")
+    assert.Equal(t, "C̅M̅L̅X̅X̅X̅V̅MMCCXXXI", IntToRoman(987231), "987231 should be C̅M̅L̅X̅X̅X̅V̅MMCCXXXI")
 }
